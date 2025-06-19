40dfdece5bd47b848f34be032bdf0b37ec688303
Subhra sundar sinha <subhrasundarsinha21@gmail.com>
2025-06-19 23:21:17 +0530

    fix(userModel): set default avatar URL for users
    The empty string default for avatar was causing issues in the UI when no avatar was provided. Using a default placeholder image ensures consistent display.

--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -50,7 +50,8 @@ const userSchema = new mongoose.Schema(
     },
     avatar: {
       type: String,
-      default: "",
+      default:
+        "https://images.unsplash.com/vector-1739804039610-efc83798ca3e?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     role: {
       type: String,
